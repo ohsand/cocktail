66e7da4d1557386b5d1ddf762112e55a1fdd541c
ohsand <osand.business@gmail.com>
2022-11-11 16:28:14 +0100

    add cocktail info section

--- src/components/cocktail.vue
+++ src/components/cocktail.vue
@@ -1,18 +1,30 @@
 <template>
     <div id="cocktails">
-        <div class="container">
+        <a href="#" class="container">
             <img :src="data.drinks[0].strDrinkThumb" />
             <h2>{{ data.drinks[0].strDrink }}</h2>
-        </div>
+        </a>
 
-        <div class="container">
+        <a href="#" class="container">
             <img :src="datatwo.drinks[0].strDrinkThumb" />
             <h2>{{ datatwo.drinks[0].strDrink }}</h2>
-        </div>
+        </a>
 
-        <div class="container">
+        <a href="#" class="container">
             <img :src="datathree.drinks[0].strDrinkThumb" />
             <h2>{{ datathree.drinks[0].strDrink }}</h2>
+        </a>
+    </div>
+
+    <div class="fullpage">
+        <div class="cocktailheader">
+            <img :src="data.drinks[0].strDrinkThumb+`/preview`" />
+                <div class="details">
+                    <h2>{{ data.drinks[0].strDrink }} <small>({{ data.drinks[0].strAlcoholic }})</small></h2>
+                    <p>Drink category: {{ data.drinks[0].strCategory }}</p>
+                    <p>Glass type: {{ data.drinks[0].strGlass }}</p>
+                    <!-- <p>Ingredients: {{ data.drinks[0].strIngredient1 }}, {{ data.drinks[0].strIngredient2 }}, {{ data.drinks[0].strIngredient3 }}, {{ data.drinks[0].strIngredient4 }}, {{ data.drinks[0].strIngredient5 }}</p> -->
+                </div>
         </div>
     </div>
 </template>
@@ -78,16 +90,52 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
     background-color: #04383F;
     color: white;
     width: 308px;
-    /* position: relative;
-    bottom: 260px;
-    height: fit-content; */
     overflow: hidden;
     position: absolute;
     bottom: -20px;
-    /* bottom: 430px; */
     border-bottom-left-radius: 8px;
     border-bottom-right-radius: 8px;
     padding: 5px 0px;
     border-top: 2px white solid;
 }
+
+.fullpage {
+    height: 100%;
+    max-width: 1125px;
+    display: flex;
+    margin: auto;
+    border-radius: 15px;
+    border: 4px solid #04383F;
+    z-index: 100;
+    background-color: #04383F;
+    box-shadow: 2px 2px 8px 0px #000000;
+}
+
+.fullpage img {
+    border-radius: 15px;
+}
+
+.cocktailheader {
+    display: flex;
+    flex-direction: row;
+    padding: 0;
+    margin: 0;
+    color: white;
+}
+
+.details {
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    margin-left: 40px;
+}
+
+.details h2 {
+    text-decoration: underline;
+}
+
+.details p {
+    margin-top: -1px;
+}
+
 </style>
