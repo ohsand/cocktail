6aba2e8b55fc47e7ed9b485d9c663ed2e447263f
ohsand <osand.business@gmail.com>
2022-11-12 10:56:42 +0100

    add details section

--- src/components/cocktail.vue
+++ src/components/cocktail.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="cocktails">
-        <a href="#" class="container">
+        <button onclick="showDiv()" class="container">
             <img :src="data.drinks[0].strDrinkThumb" />
             <h2>{{ data.drinks[0].strDrink }}</h2>
-        </a>
+        </button>
 
         <a href="#" class="container">
             <img :src="datatwo.drinks[0].strDrinkThumb" />
@@ -23,13 +23,21 @@
                     <h2>{{ data.drinks[0].strDrink }} <small>({{ data.drinks[0].strAlcoholic }})</small></h2>
                     <p>Drink category: {{ data.drinks[0].strCategory }}</p>
                     <p>Glass type: {{ data.drinks[0].strGlass }}</p>
-                    <!-- <p>Ingredients: {{ data.drinks[0].strIngredient1 }}, {{ data.drinks[0].strIngredient2 }}, {{ data.drinks[0].strIngredient3 }}, {{ data.drinks[0].strIngredient4 }}, {{ data.drinks[0].strIngredient5 }}</p> -->
+                    <p>Ingredients:</p>
+                    <p>{{ data.drinks[0].strIngredient1 }} {{ data.drinks[0].strMeasure1 }}</p>
+                    <p>{{ data.drinks[0].strIngredient2 }} {{ data.drinks[0].strMeasure2 }}</p>
+                    <p>{{ data.drinks[0].strIngredient3 }} {{ data.drinks[0].strMeasure3 }}</p>
+                    <p>{{ data.drinks[0].strIngredient4 }} {{ data.drinks[0].strMeasure4 }}</p>
+                    <p>{{ data.drinks[0].strIngredient5 }} {{ data.drinks[0].strMeasure5 }}</p>
+                    <p>{{ data.drinks[0].strIngredient6 }} {{ data.drinks[0].strMeasure6 }}</p>
                 </div>
         </div>
+        <p id="instructions">{{ data.drinks[0].strInstructions }}</p>
     </div>
 </template>
 
 <script setup>
+
 import { ref } from 'vue';
 const data = ref(null);
 const datatwo = ref(null);
@@ -52,7 +60,22 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
   
 </script>   
 
+<script>
+    export default {
+        methods: {
+        showDiv () {
+            document.getElementsByClassName('fullpage').element.style.setProperty('display', 'block');
+        }
+        }
+    }
+</script>
+
 <style>
+
+button:hover {
+    opacity: 70%;
+    cursor: pointer;
+}
 .container {
     display: flex;
     height: 310px;
@@ -62,12 +85,13 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
     font-size: medium;
     border-radius: 15px;
     border: 4px solid #04383F;
-    margin: 30px 50px;
+    margin: 30px 30px;
     overflow: hidden;
     justify-content: center;
     align-items: center;
     position: relative;
     box-shadow: 2px 2px 8px 0px #000000;
+    color: white;
 }
 
 .container img {
@@ -83,6 +107,7 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
     align-items: center;
     flex-wrap: wrap;
     margin-top: 50px;
+    margin-bottom: 40px;
     position: relative;
 }
 
@@ -101,18 +126,25 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
 
 .fullpage {
     height: 100%;
-    max-width: 1125px;
-    display: flex;
+    max-width: 1050px;
+    display: none;
     margin: auto;
     border-radius: 15px;
     border: 4px solid #04383F;
     z-index: 100;
     background-color: #04383F;
     box-shadow: 2px 2px 8px 0px #000000;
+    margin-bottom: 50px;
+    /* display: flex; */
+    flex-direction: column;
+    color: white;
+    text-align: left;
 }
 
 .fullpage img {
     border-radius: 15px;
+    height: 315px;
+    width: 315px;
 }
 
 .cocktailheader {
@@ -136,6 +168,42 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
 
 .details p {
     margin-top: -1px;
+}
+
+#instructions {
+    padding: 25px;
+}
+
+@media screen and (max-width: 1200px) {
+  .fullpage {
+    width: 80%;
+  }
+}
+
+@media screen and (max-width: 800px) {
+  .cocktailheader {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+  }
+
+  .fullpage {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 350px;
+  }
+
+  .details {
+    display: flex;
+    justify-content: center;
+    margin: 0;
+  }
+
+  #instructions {
+    margin: 0;
+  }
 }
 
 </style>
